6f322af38dae26489fcca90e340488cf7006d276
Masilevich Leanid <l.masilevich@gmail.com>
2016-12-29 16:57:17 +0300

    test

--- lib/control_date_reminder.rb
+++ lib/control_date_reminder.rb
@@ -41,7 +41,9 @@ module ControlDateReminder
 
     manager_ids = Role.find_by_name("Менеджер").members.group(:user_id).map(&:user_id)
 
-    p "manager_ids = " + manager_ids.join(",")
+    p "manager_ids = " + manager_ids.join(", ")
+    p "@user_ids = " @user_ids.join(", ")
+    p (manager_ids & @user_ids).join(", ")
 
     ids = @user_ids.present? ? (manager_ids & @user_ids) : manager_ids
 
